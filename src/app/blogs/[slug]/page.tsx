bfccfc5aba85967abed21434d5dabf9e58152911
jolene <jolenetannky@gmail.com>
2025-08-19 02:42:52 +0800

    feat: display posts and images from supabase

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -6,6 +6,9 @@ import {
 } from "../utils";
 import fs from "fs";
 
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
 type Props = {
   params: {
     slug: string;
@@ -17,49 +20,87 @@ type Post = {
   content: string;
 };
 
-const parseObsidianToMD = (content: string): string => {
-  // Find text in double brackets
-  const linkRegex = /\[\[([^\]]+)\]\]/g;
-  const imageRegex = /!\[\[([^\]]+)\]\]/g;
-
-  // Replace double brackets with single bracket links
-  const replacedImagesText = content.replace(
-    imageRegex,
-    "![](/attachments/$1)"
+const getSupabasePublicUrl = (bucketName: string, itemName: string) => {
+  return (
+    process.env.NEXT_PUBLIC_SUPABASE_URL +
+    "/storage/v1/object/public/" +
+    bucketName +
+    "/" +
+    itemName
   );
-  const replacedLinksText = replacedImagesText.replace(linkRegex, "[$1]($1)");
-
-  return replacedLinksText;
 };
 
-const getPostContent = (fname: string): string => {
-  const fullPath = getObsidianFolderPath() + "/" + fname;
-  const content = fs.readFileSync(fullPath, "utf-8");
-  const mdContent = parseObsidianToMD(content);
+/**
+ * Replaces image links with the public image URL.
+ */
+const parseContent = (content: string) => {
+  // replaces images with `![image](<publicUrlLink>)`
+  const obsidianImgRegex = /!\[\[(.*?)\]\]/g;
+  const markdownRe = /!\[([^\]]*)\]\(([^)]+)\)/g;
+  const regexes = [obsidianImgRegex, markdownRe];
 
-  return mdContent;
+  let replaced = "";
+  for (const reg of regexes) {
+    replaced = content.replace(obsidianImgRegex, (_, fileName) => {
+      const publicUrl = getSupabasePublicUrl("images", fileName);
+      return `![image](${publicUrl})`;
+    });
+  }
+
+  return replaced;
 };
 
-const getPost = (slug: string): Post => {
-  // for content, we need to parse the file
-  const fname = getFileNameFromSlug(slug);
-  const content = getPostContent(fname);
+// const parseObsidianToMD = (content: string): string => {
+//   // Find text in double brackets
+//   const linkRegex = /\[\[([^\]]+)\]\]/g;
+//   const imageRegex = /!\[\[([^\]]+)\]\]/g;
 
-  return {
-    title: getTitleFromFileName(fname),
-    content: content,
-  };
-};
+//   // Replace double brackets with single bracket links
+//   const replacedImagesText = content.replace(
+//     imageRegex,
+//     "![](/attachments/$1)"
+//   );
+//   const replacedLinksText = replacedImagesText.replace(linkRegex, "[$1]($1)");
 
-const Page = ({ params }: Props) => {
+//   return replacedLinksText;
+// };
+
+// const getPostContent = (fname: string): string => {
+//   const fullPath = getObsidianFolderPath() + "/" + fname;
+//   const content = fs.readFileSync(fullPath, "utf-8");
+//   const mdContent = parseObsidianToMD(content);
+
+//   return mdContent;
+// };
+
+// const getPost = (slug: string): Post => {
+//   // for content, we need to parse the file
+//   const fname = getFileNameFromSlug(slug);
+//   const content = getPostContent(fname);
+
+//   return {
+//     title: getTitleFromFileName(fname),
+//     content: content,
+//   };
+// };
+
+const Page = async ({ params }: Props) => {
+  const cookieStore = await cookies();
+  const supabase = await createClient(cookieStore);
   const { slug } = params;
+  const title = decodeURIComponent(slug);
 
-  const post = getPost(slug);
+  const postUrl = await supabase.storage.from("content").getPublicUrl(title)
+    .data.publicUrl;
+  const res = await fetch(postUrl);
+  const content = await res.text();
+
+  const parsedContent = parseContent(content);
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <Markdown>{post.content}</Markdown>
+      <h1>{title}</h1>
+      <Markdown>{parsedContent}</Markdown>
     </div>
   );
 };
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,32 +3,54 @@ import Link from "next/link";
 import fs from "fs";
 import { getObsidianFolderPath, getTitleFromFileName } from "./utils";
 
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
 // parse files in folder, and return titles
-const getPostMetadata = () => {
-  const folderPath = getObsidianFolderPath();
+// const getPostMetadata = () => {
+//   const folderPath = getObsidianFolderPath();
 
-  try {
-    const files = fs.readdirSync(folderPath);
-    const mdFiles = files.filter((file) => file.endsWith("md"));
-    const res = mdFiles.map((file) => ({
-      title: getTitleFromFileName(file), // remove ".md" lol
-    }));
-    return res;
-  } catch (err) {
-    console.log("Failed to get posts", err);
-    return [];
-  }
-};
+//   try {
+//     const files = fs.readdirSync(folderPath);
+//     const mdFiles = files.filter((file) => file.endsWith("md"));
+//     const res = mdFiles.map((file) => ({
+//       title: getTitleFromFileName(file), // remove ".md" lol
+//     }));
+//     return res;
+//   } catch (err) {
+//     console.log("Failed to get posts", err);
+//     return [];
+//   }
+// };
 
-const Page = () => {
-  const posts = getPostMetadata();
+const Page = async () => {
+  const cookieStore = await cookies();
+  const supabase = await createClient(cookieStore);
+
+  //   console.log("HII", await supabase.storage.listBuckets());
+  //   console.log("HII", await supabase.from("blogs").select());
+
+  const { data: posts } = await supabase.storage.from("content").list();
+
+  //   console.log("HIII", posts);
+
+  //   console.log("HII", posts, error);
+
+  //   const { data, error } = await supabase.storage
+  //     .from("content")
+  //     .download("blog 1.md");
+  //   console.log(await data?.text());
+
+  //   const posts = getPostMetadata();
+
+  // fetch post titles?
 
   return (
     <Center>
       <Stack>
-        {posts.map((post, idx) => (
-          <Link href={`/blogs/${post.title}`} key={idx}>
-            {post.title}
+        {posts?.map((post, idx) => (
+          <Link href={`/blogs/${post.name}`} key={idx}>
+            {post.name}
           </Link>
         ))}
       </Stack>
